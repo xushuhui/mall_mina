653b9ea529cc86f270a42d1edcbc5d0f3a0799da
xushuhui <474497097@qq.com>
2020-02-22 14:09:07 +0800

    update

---
+++ components/sub-category/index.js
@@ -0,0 +1,34 @@
+// components/spu-preview/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    categories: Array,
+    bannerImg: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+  observers: {
+    'categories': (t) => {
+
+    }
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onTapGridItem(event) {
+      const id = event.detail.key
+      this.triggerEvent('itemtap', {
+        cid: id,
+
+      })
+    },
+
+  }
+})
